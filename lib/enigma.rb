b4e91d4ea3f4c6028a32cad6b578228a403c8720
Brian Plantico <bplantico@gmail.com>
2019-04-15 17:42:17 -0600

    Adds new approach of putting everything into Enigma class to get it working, then will refactor later

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,13 +1,32 @@
 require 'date'
 
 class Enigma
+  attr_reader :seed_number
 
-  def encrypt(message, key, offset = Date.today.strftime("%d%m%y"))
+  def initialize
+    @seed_number = rand(0..99999)
+  end
+
+  def key(letter)
+    seed_string = @seed_number.to_s.rjust(5, "0")
+    seed_string_array = seed_string.chars
+    a,b,c,d,e = seed_string_array
+    return (a + b) if letter.downcase == 'a'
+    return (b + c) if letter.downcase == 'b'
+    return (c + d) if letter.downcase == 'c'
+    return (d + e) if letter.downcase == 'd'
+  end
+
+  def encrypt(message, key = @seed_number, date = "150419") # Date.today.strftime("%d%m%y"))
+
     encryption_hash = {
                     encryption: nil,
                     key: key,
-                    date: offset
-    }
+                    date: date
+                      }
+
   end
+
+
 
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -36,4 +36,24 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, @enigma.encrypt("hello world", "02715")
   end
 
+  def test_it_generates_seed_number
+    assert_instance_of Integer, @enigma.seed_number
+  end
+
+  def test_key_method_returns_two_char_string
+    assert_equal 2, @enigma.key('A').length
+    assert_equal String, @enigma.key('A').class
+  end
+
+  def test_key_method_returns_same_key_if_called_twice
+    assert @enigma.key('a') == @enigma.key('A')
+    assert @enigma.key('B') == @enigma.key('B')
+    assert @enigma.key('C') == @enigma.key('c')
+    assert @enigma.key('d') == @enigma.key('d')
+  end
+
+  def test_case_name
+
+  end
+
 end
