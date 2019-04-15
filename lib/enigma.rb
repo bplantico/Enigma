6d69186cee01e7354b41838afd4dd6842bb74f12
Brian Plantico <bplantico@gmail.com>
2019-04-15 11:38:16 -0600

    Adds skeleton encrypt class to Enigma class and passes test that today's date used if no date is passed in

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,4 +1,13 @@
+require 'date'
+
 class Enigma
-  
+
+  def encrypt(message, key, date = Date.today.strftime("%d%m%y"))
+    encryption_hash = {
+                    encryption: nil,
+                    key: key,
+                    date: date
+    }
+  end
 
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/emoji'
 require './lib/enigma'
 
+
 class EnigmaTest < Minitest::Test
 
   def setup
@@ -10,6 +11,28 @@ class EnigmaTest < Minitest::Test
 
   def test_it_exists
     assert_instance_of Enigma, @enigma
+  end
+
+  def test_encrypt_method_returns_hash
+
+    expected = {
+                encryption: nil,
+                key: "02715",
+                date: "040895"
+                }
+
+    assert_equal expected, @enigma.encrypt("hello world", "02715", "040895")
+  end
+
+  def test_encrypt_method_returns_today_if_no_date_given
+
+    expected = {
+                encryption: nil,
+                key: "02715",
+                date: "150419"
+                }
+
+    assert_equal expected, @enigma.encrypt("hello world", "02715")
   end
 
 end
