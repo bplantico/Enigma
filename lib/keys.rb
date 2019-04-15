16988fb721147d2326ae29ac7fe1b911f6cb3aca
Brian Plantico <bplantico@gmail.com>
2019-04-15 13:00:01 -0600

    Adds seed_number and seed_string methods to Keys class

--- lib/keys.rb
+++ lib/keys.rb
@@ -1,4 +1,17 @@
 class Keys
+
+  def seed_number
+    rand(0..99999)
+  end
+
+  def seed_string
+    seed_string = seed_number.to_s
+    until seed_string.length == 5
+      seed_string = "0" + "#{seed_string}"
+    end
+    seed_string
+  end
+
   
 
 end
--- test/keys_test.rb
+++ test/keys_test.rb
@@ -13,4 +13,12 @@ class KeysTest < Minitest::Test
     assert_instance_of Keys, @keys
   end
 
+  def test_seed_number_is_an_integer
+    assert_equal Integer, @keys.seed_number.class
+  end
+
+  def test_seed_becomes_five_char_string
+    assert_equal 5, @keys.seed_string.length
+  end
+
 end
